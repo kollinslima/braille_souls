54d9d7d2578a377a20babb49b310186fc9c0bcf2
kollinslima <kollins.lima@usp.br>
2018-03-20 18:14:40 -0300

    Tela inicial criada

--- brailler/app/src/main/java/com/example/sumi/brailler/MainActivity.java
+++ brailler/app/src/main/java/com/example/sumi/brailler/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sumi.brailler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickStartButton(View view) {
+        return;
+    }
+
+    public void onClickDictionatyButton(View view) {
+        return;
+    }
+
+    public void onClickExitButton(View view) {
+        finish();
     }
 }
