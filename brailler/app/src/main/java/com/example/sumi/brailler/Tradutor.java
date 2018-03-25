d19ba4f6162a8bd4b17634d601f72ffa629545af
Otávio Sumi <otaviosumi@hotmail.com>
2018-03-24 22:27:46 -0300

    Ainda no tradutor

--- brailler/app/src/main/java/com/example/sumi/brailler/Tradutor.java
+++ brailler/app/src/main/java/com/example/sumi/brailler/Tradutor.java
@@ -2,6 +2,7 @@ package com.example.sumi.brailler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Tradutor extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Tradutor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tradutor);
+    }
+
+    public void onTrocaClick(View view) {
     }
 }
