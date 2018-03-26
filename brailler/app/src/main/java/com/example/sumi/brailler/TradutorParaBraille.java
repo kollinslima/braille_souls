28597358e07c5e09e9aaf7c388881d644099bff3
Otavio Sumi <otaviosumi@hotmail.com>
2018-03-26 18:44:39 -0300

    Lista ainda com alguma coisa errada (quando adiciona)

--- brailler/app/src/main/java/com/example/sumi/brailler/TradutorParaBraille.java
+++ brailler/app/src/main/java/com/example/sumi/brailler/TradutorParaBraille.java
@@ -3,6 +3,14 @@ package com.example.sumi.brailler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TradutorParaBraille extends AppCompatActivity {
 
@@ -10,6 +18,27 @@ public class TradutorParaBraille extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tradutor_para_braille);
+        final ListView lv = (ListView) findViewById(R.id.tradutorListView);
+        final Button btn = (Button) findViewById(R.id.buttonTraduzir);
+        final TextView tv = (TextView) findViewById(R.id.tradutorCampoParaBraille);
+        final List<String> braille_list = new ArrayList<String>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, braille_list);
+        lv.setAdapter(arrayAdapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                braille_list.add("01010101");
+                arrayAdapter.notifyDataSetChanged();
+                /*
+                String palavra = tv.getText().toString();
+                braille_list.clear();
+                for(int i = 0; i < palavra.length(); i++){
+                    braille_list.add(MenuPrincipal.text_to_braille.get(palavra.charAt(i)));
+                }
+                arrayAdapter.notifyDataSetChanged();*/
+            }
+        });
     }
 
 
