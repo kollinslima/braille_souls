d8eeecff97a1ccffca384c2e303d3dba9f7409e1
Otavio Sumi <otaviosumi@hotmail.com>
2018-03-29 12:41:35 -0300

    Tradutor - braille 2 text - acho que essa parte ta funcional agr

--- brailler/app/src/main/java/com/example/sumi/brailler/TradutorDoBraille.java
+++ brailler/app/src/main/java/com/example/sumi/brailler/TradutorDoBraille.java
@@ -2,31 +2,32 @@ package com.example.sumi.brailler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.widget.Toast;
 
+import java.util.Map;
+
 public class TradutorDoBraille extends AppCompatActivity {
 
     private ToggleButton[] tecladoBraile = new ToggleButton[6];
     private Switch numLock;
-    private String resultadoCampo;
-    private TextView resultadoCampoView;
+    private String campoDig, campoCarac;
+    private TextView campoDigView, campoCaracView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tradutor_do_braille);
-        resultadoCampoView = (TextView) findViewById(R.id.resultadoCampo);
+        campoDigView = (TextView) findViewById(R.id.campoDigito);
+        campoCaracView = (TextView) findViewById(R.id.campoCaractere);
         tecladoBraile[0] = (ToggleButton) findViewById(R.id.braille_button_1x1);
         tecladoBraile[1] = (ToggleButton) findViewById(R.id.braille_button_1x2);
         tecladoBraile[2] = (ToggleButton) findViewById(R.id.braille_button_2x1);
         tecladoBraile[3] = (ToggleButton) findViewById(R.id.braille_button_2x2);
         tecladoBraile[4] = (ToggleButton) findViewById(R.id.braille_button_3x1);
         tecladoBraile[5] = (ToggleButton) findViewById(R.id.braille_button_3x2);
-        numLock = (Switch) findViewById(R.id.button_numLock);
     }
 
     public void onClickAdicionar(android.view.View view) {
@@ -41,18 +42,28 @@ public class TradutorDoBraille extends AppCompatActivity {
             button.setChecked(false);
         }
         if(MenuPrincipal.braille_to_text.get(resposta) == null){
-             Toast.makeText(this, "Desconhecido", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Desconhecido", Toast.LENGTH_SHORT).show();
             return;
         }
-        resultadoCampo = resultadoCampoView.getText().toString();
-        resultadoCampo = resultadoCampo + MenuPrincipal.braille_to_text.get(resposta);
-        resultadoCampoView.setText(resultadoCampo);
+        for(Map.Entry<String, String> it : MenuPrincipal.text_to_braille.entrySet()){
+            if(it.getValue().equals(resposta)){
+                if(Character.isDigit(it.getKey().charAt(0))){
+                    campoDig = campoDigView.getText().toString();
+                    campoDig = campoDig + it.getKey();
+                    campoDigView.setText(campoDig);
+                }else{
+                    campoCarac = campoCaracView.getText().toString();
+                    campoCarac = campoCarac + it.getKey();
+                    campoCaracView.setText(campoCarac);
+                }
+            }
+        }
     }
 
     public void onClickAdicionarEspaco(android.view.View view) {
-        resultadoCampo = resultadoCampoView.getText().toString();
-        resultadoCampo = resultadoCampo + " ";
-        resultadoCampoView.setText(resultadoCampo);
+        campoDig = campoDigView.getText().toString();
+        campoDig = campoDig + " ";
+        campoDigView.setText(campoDig);
         for (ToggleButton button : tecladoBraile) {
             button.setChecked(false);
         }
@@ -61,5 +72,6 @@ public class TradutorDoBraille extends AppCompatActivity {
     public void onClickVoltarTradutor(android.view.View view) {
         finish();
     }
+
 }
 
