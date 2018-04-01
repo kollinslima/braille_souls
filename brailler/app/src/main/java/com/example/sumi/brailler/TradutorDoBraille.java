1a6325f16632388b5b550c959d423242eb47705a
Otavio Sumi <otaviosumi@hotmail.com>
2018-04-01 13:34:49 -0300

    Tradutor - braille 2 text - [Caracter/Digito] implementado com novo Map

--- brailler/app/src/main/java/com/example/sumi/brailler/TradutorDoBraille.java
+++ brailler/app/src/main/java/com/example/sumi/brailler/TradutorDoBraille.java
@@ -13,15 +13,14 @@ public class TradutorDoBraille extends AppCompatActivity {
 
     private ToggleButton[] tecladoBraile = new ToggleButton[6];
     private Switch numLock;
-    private String campoDig, campoCarac;
-    private TextView campoDigView, campoCaracView;
+    private String campoRes;
+    private TextView campoResView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tradutor_do_braille);
-        campoDigView = (TextView) findViewById(R.id.campoDigito);
-        campoCaracView = (TextView) findViewById(R.id.campoCaractere);
+        campoResView = (TextView) findViewById(R.id.campoResultado);
         tecladoBraile[0] = (ToggleButton) findViewById(R.id.braille_button_1x1);
         tecladoBraile[1] = (ToggleButton) findViewById(R.id.braille_button_1x2);
         tecladoBraile[2] = (ToggleButton) findViewById(R.id.braille_button_2x1);
@@ -41,29 +40,26 @@ public class TradutorDoBraille extends AppCompatActivity {
             }
             button.setChecked(false);
         }
-        if(MenuPrincipal.braille_to_text.get(resposta) == null){
+        campoRes = campoResView.getText().toString();
+        resposta = MenuPrincipal.braille_to_text.get(resposta).toString();
+        if(resposta.equals("[]")){
             Toast.makeText(this, "Desconhecido", Toast.LENGTH_SHORT).show();
             return;
         }
-        for(Map.Entry<String, String> it : MenuPrincipal.text_to_braille.entrySet()){
-            if(it.getValue().equals(resposta)){
-                if(Character.isDigit(it.getKey().charAt(0))){
-                    campoDig = campoDigView.getText().toString();
-                    campoDig = campoDig + it.getKey();
-                    campoDigView.setText(campoDig);
-                }else{
-                    campoCarac = campoCaracView.getText().toString();
-                    campoCarac = campoCarac + it.getKey();
-                    campoCaracView.setText(campoCarac);
-                }
-            }
+        if(resposta.length() > 3){
+            resposta = Character.toString(resposta.charAt(1)) + "/" + Character.toString(resposta.charAt(4));
+            campoRes = campoRes + resposta;
+        }else{
+            resposta = Character.toString(resposta.charAt(1));
+            campoRes = campoRes + resposta;
         }
+        campoResView.setText(campoRes);
     }
 
     public void onClickAdicionarEspaco(android.view.View view) {
-        campoDig = campoDigView.getText().toString();
-        campoDig = campoDig + " ";
-        campoDigView.setText(campoDig);
+        campoRes = campoResView.getText().toString();
+        campoRes = campoRes + " ";
+        campoResView.setText(campoRes);
         for (ToggleButton button : tecladoBraile) {
             button.setChecked(false);
         }
