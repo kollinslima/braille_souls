b4eb55058c5ccdd5d35d725d62106419c9b45658
Otavio Sumi <otaviosumi@hotmail.com>
2018-04-06 19:58:32 -0300

    .

---
+++ brailler/app/src/main/java/com/example/sumi/brailler/game/LearningMode.java
@@ -0,0 +1,13 @@
+package com.example.sumi.brailler.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LearningMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_mode);
+    }
+}
