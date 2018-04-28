23a0242c7eae0ef414838d9cd0a78dde9f9dd2f8
Otavio Sumi <otaviosumi@hotmail.com>
2018-04-28 17:58:40 -0300

    player name not found

--- braille_souls/app/src/main/java/com/example/sumi/brailler/user_profile/UserProfileActivity.java
+++ braille_souls/app/src/main/java/com/example/sumi/brailler/user_profile/UserProfileActivity.java
@@ -1,6 +1,43 @@
 package com.example.sumi.brailler.user_profile;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.sumi.brailler.R;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        TextView userName = findViewById(R.id.playerName);
+        //Not working
+        userName.setText(getEmiailID(getApplicationContext()));
+    }
+
+    private String getEmiailID(Context context) {
+        AccountManager accountManager = AccountManager.get(context);
+        Account account = getAccount(accountManager);
+        if (account == null) {
+            return null;
+        } else {
+            return account.name;
+        }
+    }
+
+    private static Account getAccount(AccountManager accountManager) {
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        Account account;
+        if (accounts.length > 0) {
+            account = accounts[0];
+        } else {
+            account = null;
+        }
+        return account;
+    }
 }
