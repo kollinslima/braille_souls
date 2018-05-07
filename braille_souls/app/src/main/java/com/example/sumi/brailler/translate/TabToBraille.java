3da14bd00134885676c5567c34afa0613f3356cc
Otavio Sumi <otaviosumi@hotmail.com>
2018-05-07 11:34:28 -0300

    view prepared

--- braille_souls/app/src/main/java/com/example/sumi/brailler/translate/TabToBraille.java
+++ braille_souls/app/src/main/java/com/example/sumi/brailler/translate/TabToBraille.java
@@ -7,14 +7,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.sumi.brailler.R;
 
 public class TabToBraille extends Fragment{
 
+    private EditText input;
+    private Button buttonTranslate;
+    private GridView gridView;
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_to_braille, container, false);
+        view = inflater.inflate(R.layout.fragment_to_braille, container, false);
+        inicializer();
+        return view;
+    }
+
+    private void inicializer(){
+        input = (EditText)view.findViewById(R.id.translatorTextFieldInput);
+        buttonTranslate = (Button)view.findViewById(R.id.translateButton);
+        gridView = (GridView)view.findViewById(R.id.gridviewToBraille);
+
+        buttonTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), getResources().getString(R.string.not_implemented_yet),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
